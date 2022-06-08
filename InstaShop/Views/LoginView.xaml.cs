8a5bb61824e1962acd8886d294d9c7754faf9787
kelidis <nkelidhs@gmail.com>
2022-06-08 20:52:33 +0300

    Add project files.

---
+++ InstaShop/Views/LoginView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InstaShop
+{
+    /// <summary>
+    /// Interaction logic for LoginForm.xaml
+    /// </summary>
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void userName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void password_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
